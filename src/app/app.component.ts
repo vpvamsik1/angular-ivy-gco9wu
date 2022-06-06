25c57557cebc992016fd4e7e1549a03767f5fa8a
vpvamsik1 <vpvamsik1@gmail.com>
2022-06-06 15:00:38 -0700

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular 2' + VERSION.major;
+export class AppComponent {
+  name = 'Angular 3' + VERSION.major;
 }
