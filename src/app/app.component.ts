fca93d7c88c42a22bd854f5a458edef269a5cacd
vpvamsik1 <vpvamsik1@gmail.com>
2022-06-09 12:13:55 -0700

    added 'map,'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, VERSION } from '@angular/core';
-import { of, from } from 'rxjs';
+import { of, from, map, tap, take } from 'rxjs';
 
 @Component({
   selector: 'my-app',
@@ -10,24 +10,38 @@ export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
 
   ngOnInit() {
-    of(2, 4, 6, 8).subscribe((item) => console.log(item));
+    // of(2, 4, 6, 8).subscribe((item) => console.log(item));
 
-    from([20, 15, 10, 5]).subscribe({
-      next: (item) => console.log(`resulting item .. ${item}`),
-      error: (err) => console.log(`error occurred ${err}`),
-      complete: () => console.log('complete'),
-    });
+    from([20, 15, 10, 5])
+      .pipe(
+        tap((item) => console.log('emitted item...' + item)),
+        map((item) => item * 2),
+        map((item) => item - 10),
+        map((item) => {
+          if (item === 0) {
+            throw new Error('zero detected');
+          }
+          return item;
+        }),
+        take(3)
+        // you do not see error anymore because '0' is not being logged anymore because we are only taking the first 3 values
+      )
+      .subscribe({
+        next: (item) => console.log(`resulting item .. ${item}`),
+        error: (err) => console.log(`error occurred ${err}`),
+        complete: () => console.log('complete'),
+      });
 
-    of('Apple 1', 'Apple 2', 'Apple 3').subscribe({
-      next: (apple) => console.log(`Apple emitted ${apple}`),
-      error: (err) => console.log(`Error occurred: ${err}`),
-      complete: () => console.log(`No more apples, go home`),
-    });
+    // of('Apple 1', 'Apple 2', 'Apple 3').subscribe({
+    //   next: (apple) => console.log(`Apple emitted ${apple}`),
+    //   error: (err) => console.log(`Error occurred: ${err}`),
+    //   complete: () => console.log(`No more apples, go home`),
+    // });
 
-    from(['Apple 4', 'Apple 5', 'Apple 6']).subscribe({
-      next: (apple) => console.log(`Apple emitted ${apple}`),
-      error: (err) => console.log(`Error occurred: ${err}`),
-      complete: () => console.log(`No more apples, go home 2`),
-    });
+    // from(['Apple 4', 'Apple 5', 'Apple 6']).subscribe({
+    //   next: (apple) => console.log(`Apple emitted ${apple}`),
+    //   error: (err) => console.log(`Error occurred: ${err}`),
+    //   complete: () => console.log(`No more apples, go home 2`),
+    // });
   }
 }
